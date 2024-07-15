d4d8c7cf2beab81a385e30b6c0640041b1ba5f56
RodrigoPaulaLopes <rodrigo.plopesti@gmail.com>
2024-07-15 00:48:57 -0300

    test: :test_tube: create test to create new customers

--- jest.config.ts
+++ jest.config.ts
@@ -6,12 +6,36 @@
 import type {Config} from 'jest';
 
 const config: Config = {
-  collectCoverage: true,
-  coverageDirectory: "coverage",
+  // All imported modules in your tests should be mocked automatically
+  // automock: false,
+
+  // Stop running tests after `n` failures
+  // bail: 0,
+
+  // The directory where Jest should store its cached dependency information
+  // cacheDirectory: "/tmp/jest_rs",
+
+  // Automatically clear mock calls, instances, contexts and results before every test
+  clearMocks: true,
+
+  // Indicates whether the coverage information should be collected while executing the test
+  // collectCoverage: false,
+
+  // An array of glob patterns indicating a set of files for which coverage information should be collected
+  // collectCoverageFrom: undefined,
+
+  // The directory where Jest should output its coverage files
+  // coverageDirectory: undefined,
+
+  // An array of regexp pattern strings used to skip coverage collection
   // coveragePathIgnorePatterns: [
   //   "/node_modules/"
   // ],
+
+  // Indicates which provider should be used to instrument code for coverage
   coverageProvider: "v8",
+
+  // A list of reporter names that Jest uses when writing coverage reports
   // coverageReporters: [
   //   "json",
   //   "text",
@@ -122,7 +146,7 @@ const config: Config = {
   // snapshotSerializers: [],
 
   // The test environment that will be used for testing
-  testEnvironment: "node",
+  // testEnvironment: "jest-environment-node",
 
   // Options that will be passed to the testEnvironment
   // testEnvironmentOptions: {},
---
+++ src/modules/customers/services/createCustomerService.spec.ts
@@ -0,0 +1,10 @@
+describe('Create customer service', () => {
+    it('Should be able to create a new customer', () => {
+        expect(5 + 5).toEqual(10)
+    })
+
+    it('Should not be able to create two customer with the same email', () => {
+        expect(5 + 5).toEqual(10)
+    })
+})
+
